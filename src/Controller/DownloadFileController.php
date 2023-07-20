f29bc209decc4d246252528f5b5b6546a753a49f
Mikhail Shelestov <m.shelestov@trackad.ai>
2023-07-20 21:42:04 +0400

    - Added left menu sidebar  - implemented reader  - beautified forms and templates with bootstrap

--- src/Controller/DownloadFileController.php
+++ src/Controller/DownloadFileController.php
@@ -2,17 +2,52 @@
 
 namespace App\Controller;
 
+use App\Form\BookDownloadFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DownloadFileController extends AbstractController
 {
     #[Route('/download/file', name: 'app_download_file')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(BookDownloadFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $file */
+            $file = $form->get('file')->getData();
+
+            //TODO move all save file functionality to separate service
+
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+
+                $newFilename = sprintf('%s%s%s', $originalFilename, uniqid(), $file->guessExtension());
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $file->move(
+                        'books',
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    dd($e->getMessage());
+                }
+
+                $fileUploaded = true;
+            }
+        }
+
         return $this->render('download_file/index.html.twig', [
             'controller_name' => 'DownloadFileController',
+            'form' => $form->createView(),
+            'fileUploaded' => $fileUploaded ?? false,
         ]);
     }
 }
--- src/Controller/ReaderController.php
+++ src/Controller/ReaderController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,9 +12,26 @@ class ReaderController extends AbstractController
     #[Route('/', name: 'app_reader')]
     public function index(): Response
     {
+        $file = new Filesystem();
+
+        $fileWeWannaRead = 'books/kek-64b9578a20474.txt';
+
+        if ($file->exists('books/kek-64b9578a20474.txt')) {
+            $text = file_get_contents($fileWeWannaRead);
+        } else {
+            $text = file_get_contents('test_text.txt');
+        }
+
+        $filteredText = trim(str_replace([PHP_EOL, "\t", "\n", '"', "'"], [' ', ' ', ' ', '`', '`'], $text));
+
         return $this->render('reader/index.html.twig', [
             'controller_name' => 'ReaderController',
-            'text' => ['This', 'is', 'text', 'array']
+            'text' => array_values(
+                array_filter(
+                    explode(' ', $filteredText),
+                    function ($item) { return $item !== ''; }
+                )
+            ),
         ]);
     }
 }
